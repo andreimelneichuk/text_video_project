5748d69205578076f075716f3e1d64412217867a
Андрей Мельнейчук <melaiia@icloud.com>
2024-09-01 16:44:09 +0300

    sqllite

--- text_video_project/settings.py
+++ text_video_project/settings.py
@@ -88,15 +88,22 @@ STATICFILES_DIRS = [
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# postrge sql
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'mydatabase',
+#         'USER': 'myuser',
+#         'PASSWORD': 'mypassword',
+#         'HOST': 'db',  # Ссылка на сервис базы данных из Docker Compose
+#         'PORT': '5432',
+#     }
+# }
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydatabase',
-        'USER': 'myuser',
-        'PASSWORD': 'mypassword',
-        'HOST': 'db',  # Ссылка на сервис базы данных из Docker Compose
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
